625f1191b94152eafed7ae7b3d0f1c4335ea04b2
Brett Crawford <brettjamescrawford@gmail.com>
2014-10-17 07:10:08 -0400

    Created shared buffer class

---
+++ src/BC_Buffer/BC_Buffer.cpp
@@ -0,0 +1,95 @@
+/* File: BC_Buffer.cpp
+   Author: Brett Crawford
+   Date: 2014-
+   Prof: Kwatny
+   TAs: Liang and Casey
+   Course: CIS 3207, Sec 2
+   Description:
+*/
+
+using namespace std;
+
+#include "BC_Buffer.h"
+#include <semaphore.h>
+
+
+BC_Buffer::BC_Buffer(int size, BC_Logger *logger)
+{
+	first = last = 0;
+	this->size = size;
+	this->logger = logger;
+	buffer = (int*) calloc(this->size, sizeof(int));
+	pthread_mutexattr_init(&attr);
+	pthread_mutex_init(&lock, &attr);
+	sem_init(&available, 0, this->size);
+	sem_init(&unavailable, 0, 0);
+}
+
+BC_Buffer::~BC_Buffer()
+{
+	free(buffer);
+	pthread_mutex_destroy(&lock);
+	sem_destroy(&available);
+	sem_destroy(&unavailable);
+}
+
+/**
+ * Checks if the buffer is full.
+ * Returns 1 if the buffer is full, otherwise 0
+ */
+int BC_Buffer::isFull()
+{
+	int result = 0;
+	pthread_mutex_lock(&lock);
+	if((last - first) == size)
+		result = 1;
+	pthread_mutex_unlock(&lock);
+
+	return result;
+}
+
+int BC_Buffer::isEmpty()
+{
+	int result = 0;
+	pthread_mutex_lock(&lock);
+	if((last - first == 0))
+		result = 1;
+	pthread_mutex_unlock(&lock);
+
+	return result;
+}
+
+void BC_Buffer::insert(int item)
+{
+	int num;
+	char *event = (char*) calloc(50, sizeof(char));
+	sem_wait(&available);
+	pthread_mutex_lock(&lock);
+	buffer[last % size] = item;
+	last++;
+	num = last - first;
+	pthread_mutex_unlock(&lock);
+	sem_post(&unavailable);
+	sprintf(event, "Buffer: %d inserted, num: %d, first: %d, last: %d", item, num, first, last);
+	logger->log_event(event);
+	free(event);
+}
+
+int BC_Buffer::remove()
+{
+	int num;
+	char *event = (char*) calloc(50, sizeof(char));
+	sem_wait(&unavailable);
+	pthread_mutex_lock(&lock);
+	int item = buffer[first % size];
+	first++;
+	num = last - first;
+	pthread_mutex_unlock(&lock);
+	sem_post(&available);
+	sprintf(event, "Buffer: %d removed, num: %d, first: %d, last: %d", item, num, first, last);
+	logger->log_event(event);
+	free(event);
+
+	return item;
+}
+
---
+++ src/BC_Buffer/BC_Buffer.h
@@ -0,0 +1,42 @@
+/* File: BC_Buffer.h
+   Author: Brett Crawford
+   Date: 2014-
+   Prof: Kwatny
+   TAs: Liang and Casey
+   Course: CIS 3207, Sec 2
+   Description:
+*/
+
+#ifndef BC_BUFFER_H_
+#define BC_BUFFER_H_
+
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#include "../BC_Logger/BC_Logger.h"
+
+class BC_Buffer
+{
+	private:
+		int first;
+		int last;
+		int size;
+		int *buffer;
+		BC_Logger *logger;
+		pthread_mutexattr_t attr;
+		pthread_mutex_t lock;    /* Mutual exclusion lock */
+		sem_t available;         /* Number of available slots in buffer */
+		sem_t unavailable;       /* Number of unavailable slots in buffer */
+
+	public:
+		BC_Buffer(int, BC_Logger*);
+		virtual ~BC_Buffer();
+
+		int isFull();
+		int isEmpty();
+		void insert(int);
+		int remove();
+};
+
+#endif /* BC_BUFFER_H_ */
