0d8d752d5bba5e549411f64752764aa7019533eb
Brett Crawford <brettjamescrawford@gmail.com>
2014-10-17 07:11:41 -0400

    Updated test driver

--- src/BC_Pro_Con.cpp
+++ src/BC_Pro_Con.cpp
@@ -6,17 +6,76 @@
    Course: CIS 3207, Sec 2
    Description:
 */
+
+#include "BC_Buffer/BC_Buffer.h"
+#include "BC_Consumer/BC_Consumer.h"
 #include "BC_Logger/BC_Logger.h"
+#include "BC_Producer/BC_Producer.h"
+
 #include <iostream>
+#include <pthread.h>
+#include <stdio.h>
 
 using namespace std;
 
+#define BUFFER_SIZE 20
+
+const char log_file[] = "log.txt";
+
+void pro_con_solution();
+void *produce(BC_Producer*);
+void *consume(BC_Consumer*);
 void logger_test();
 
 int main(int argc, char **argv)
 {
-	logger_test();
+	pro_con_solution();
 	return EXIT_SUCCESS;
+}
+
+void pro_con_solution()
+{
+	printf("Main thread Start\n");
+	BC_Logger *logger;
+	BC_Buffer *buffer;
+	BC_Producer *producer;
+	BC_Consumer *consumer;
+	pthread_t threads[2];
+
+	logger = new BC_Logger(log_file);
+	buffer = new BC_Buffer(BUFFER_SIZE, logger);
+	producer = new BC_Producer(0, buffer, logger);
+	consumer = new BC_Consumer(0, buffer, logger);
+
+	if(pthread_create(&(threads[0]), NULL, (void* (*)(void*)) &produce, producer))
+	{
+		perror("");
+		exit(-1);
+	}
+	if(pthread_create(&(threads[1]), NULL, (void* (*)(void*)) &consume, consumer))
+		{
+			perror("");
+			exit(-1);
+		}
+	pthread_join(threads[0], NULL);
+	pthread_join(threads[1], NULL);
+	printf("Main thread finished\n");
+}
+
+void *produce(BC_Producer *producer)
+{
+	int i;
+	for(i = 0; i < 25; i++)
+		producer->produce();
+	pthread_exit(NULL);
+}
+
+void *consume(BC_Consumer *consumer)
+{
+	int i;
+	for(i = 0; i < 25; i++)
+		consumer->consume();
+	pthread_exit(NULL);
 }
 
 void logger_test()
