05ae7efd346dd14d8956636c1ae99f6220e4e0ee
Brett Crawford <brettjamescrawford@gmail.com>
2014-10-31 19:37:47 -0400

    More formatting

--- src/BC_Producer/BC_Producer.cpp
+++ src/BC_Producer/BC_Producer.cpp
@@ -39,7 +39,7 @@ BC_Producer::~BC_Producer()
 }
 
 /**
- * Produces a pseudo-randomly generated number between 0 and 9999,
+ * Produces a pseudo-randomly generated number between 0 and 99,
  * stores its value in dynamically allocated memory, inserts a 
  * pointer to the value into the buffer, and logs the event with
  * logger.
@@ -52,8 +52,7 @@ void BC_Producer::produce()
 	*item = rand() % 100;
 	int temp = *item;
 	buffer->insert((void*) item);
-	snprintf(event, 62, "Pro %2d: %2d produced", 
-			 this->id, temp);
+	snprintf(event, 62, "Pro %2d: %2d produced", this->id, temp);
 	logger->log_event(event);
 	free(event);
 }
