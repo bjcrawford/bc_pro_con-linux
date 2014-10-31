53884dba747dcc531a869bf358e95bc6d2f09eb7
Brett Crawford <brettjamescrawford@gmail.com>
2014-10-31 19:19:05 -0400

    Moved critical section entry to provide more accurate timestamps

--- src/BC_Logger/BC_Logger.cpp
+++ src/BC_Logger/BC_Logger.cpp
@@ -41,13 +41,15 @@ BC_Logger::~BC_Logger()
 */
 int BC_Logger::log_event(const char event[])
 {
+	pthread_mutex_lock(&lock);
+	/** CRITICAL SECTION ENTRY */
+
 	/* String to hold date and time (date/time string) */
 	char *dts = (char*) calloc(21, sizeof(char));
 	/* String to hold microseconds */
 	char *ms = (char*) calloc(7, sizeof(char));
 	/* String to hold date, time, microseconds, and event  (full string)*/
-	int fs_size = 218;
-	char *fs = (char*) calloc(fs_size, sizeof(char));
+	char *fs = (char*) calloc(218, sizeof(char));
 
 	/* Get date and time */
 	time_t now = time(NULL);
@@ -66,9 +68,8 @@ int BC_Logger::log_event(const char event[])
 	strcat(fs, "\n");
 
 	/* Write event to log file */
-	pthread_mutex_lock(&lock);
-	/** CRITICAL SECTION ENTRY */
 	fwrite(fs, sizeof(char), strlen(fs), lfp);
+
 	/** CRITICAL SECTION EXIT */
 	pthread_mutex_unlock(&lock);
 
